05b3c412f56f8b0a82b9fa66699d30aa341f6b81
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-09 14:00:31 -0300

    feat: adiciona uuid como id de categoria

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+
 app.use(express.json());
-app.use(categoriesRoutes);
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3334, () => console.log("Server is Running!!!"));
