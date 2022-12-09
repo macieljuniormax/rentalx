573c91cda3bfd27cea89e4e6aa683f1626e809b3
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-09 17:17:26 -0300

    feat: garante que uma categoria já existente não seja cadastrada novamente

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExistis = categoriesRepository.findByName(name);
+  if (categoryAlreadyExistis) {
+    return response.status(400).json({ error: "Category Already Existis" });
+  }
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
