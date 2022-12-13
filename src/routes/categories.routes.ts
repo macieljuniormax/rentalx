53b4a15b7036ff6e011d12a450acf929760a230c
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-12 22:46:04 -0300

    feat: cria rota par upload de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,4 +16,8 @@ categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
 
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+});
 export { categoriesRoutes };
