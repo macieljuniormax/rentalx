689f0302f13afbf9325be587029a1a0f21cec8c6
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-20 18:32:02 -0300

    feat: configura o docker com database postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,13 +2,19 @@ import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "database_rentalx",
   port: 5432,
   username: "docker",
   password: "ignite",
-  database: "rentalX",
+  database: "database_rentalx",
   synchronize: true,
   logging: false,
 });
 
-AppDataSource.initialize();
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
